2674e30156cd00128b974631ad076137e182e7e3
EliAyase <lovelivesiftw@gmail.com>
2015-08-27 17:17:49 +0800

    JS

---
+++ ReadPredCN.js
@@ -0,0 +1,41 @@
+
+var ReadPredCN={
+	obj:"",
+	JSONpath:"http://lovelive.oss-cn-beijing.aliyuncs.com/Data/EventCard.txt",
+	init:function(){
+		var text = ReadPredCN.readTextFile(ReadPredCN.JSONpath);
+		this.obj = JSON.parse(text);
+	},
+	Read:function(num){
+		var obj = this.obj;
+		if (obj.EventGlobal[1][0] == "0")
+			return 0
+		var tmp = obj.EventGlobal[1][2+num];
+		if(tmp.indexOf("~")>-1){
+			var temp = tmp.split("~");
+			var result = parseFloat(temp[0]) + parseFloat(temp[1]);
+			result = Math.floor(result*500);
+		}
+		else if (tmp=="")
+			return 0;
+		else{
+			var result = Math.floor(parseFloat(tmp)*1000);
+		}
+		return result
+	},
+	readTextFile:function(url){
+		var rawFile = new XMLHttpRequest();
+		var allText = "";
+		rawFile.open("GET", url, false);
+		rawFile.onreadystatechange = function (){
+			if(rawFile.readyState === 4){
+				if(rawFile.status === 200 || rawFile.status == 0){
+					allText = rawFile.responseText;
+				}
+			}
+		}
+		rawFile.send(null);
+		return allText;
+	}
+}
+ReadPredCN.init()
